96309177ec569fc26490b2dc8b0ae87398053f11
takalenny-blip <taka.lenny@gmail.com>
2026-09-24 19:11:03 +0900

    test: 空H3改行正規化を追加

--- tests/test_blogger_h3_spacing.py
+++ tests/test_blogger_h3_spacing.py
@@ -12,3 +12,11 @@ def test_multiple_blank_paragraphs_are_collapsed():
 def test_existing_single_blank_paragraph_is_preserved():
     html = "<p>前</p><p><br /></p><h3>見出し</h3>"
     assert normalize_h3_spacing(html) == html
+
+def test_empty_h3_spacing_marker_becomes_blank_paragraph():
+    html = '<p>前</p><h3 id="toc-1"><br /></h3><h3 id="toc-1">見出し</h3>'
+    assert normalize_h3_spacing(html) == '<p>前</p><p><br /></p><h3 id="toc-1">見出し</h3>'
+
+def test_empty_h3_with_whitespace_is_removed():
+    html = '<h3 id="toc-1"> \n<br />\n </h3>\n<h3 id="toc-1">見出し</h3>'
+    assert normalize_h3_spacing(html) == '<p><br /></p><h3 id="toc-1">見出し</h3>'
